609e4219213f8698798b5d59bd1ce567c05d9af7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-23 16:02:33 -0300

    refactor: adiciona loading

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
@@ -8,7 +9,7 @@ import { useAuth } from "../context/AuthContext";
 import IconToggle from "./IconToggle";
 import "./NewArrivals.css";
 import NewArrivalsSkeleton from "./NewArrivalsSkeleton";
-import CircularProgress from "@mui/material/CircularProgress"; // Importar CircularProgress
+import zIndex from "@mui/material/styles/zIndex";
 import { useConfig } from "../context/ConfigContext";
 import { logPageView } from "../../analytics";
 import { Helmet } from "react-helmet";
@@ -17,31 +18,32 @@ const NewArrivals = ({ onNewArrivalsUpdate }) => {
   const [newArrivals, setNewArrivals] = useState([]);
   const [totalProducts, setTotalProducts] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const { logout, loggedIn } = useAuth();
-  const [loading, setLoading] = useState(true); // Inicialmente true para exibir o spinner
-  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const { logout, loggedIn } = useAuth(); // Obtendo o userId do contexto de autenticação
+  const [loading, setLoading] = useState(true);
+
+  const [hoveredIndex, setHoveredIndex] = useState(-1); // -1 indica que nenhuma imagem está sendo hoverada
   const { apiUrl } = useConfig();
   const location = useLocation();
 
   useEffect(() => {
     logPageView();
   }, [location]);
-
   useEffect(() => {
     const fetchSearchResults = async () => {
-      setLoading(true); // Define o loading como true ao iniciar a busca
       try {
+        // await new Promise((resolve) => setTimeout(resolve, 10000));
         const response = await fetch(
           `${apiUrl}/api/products/new-arrivals?page=${currentPage}`
         );
         const data = await response.json();
+        console.log("Data from server:", data);
         setNewArrivals(data.newArrivals);
         setTotalProducts(data.totalProducts);
-        onNewArrivalsUpdate(data.newArrivals);
+        onNewArrivalsUpdate(data.newArrivals); // Atualiza o estado no componente pai
 
-        setLoading(false); // Define loading como false após obter os dados
+        setLoading(false); // Define loading como falso após obter os dados
       } catch (error) {
-        setLoading(false); // Define loading como false mesmo em caso de erro
+        setLoading(false); // Definir carregamento como falso após tentativa de buscar dados
         console.error("Erro ao buscar resultados de pesquisa:", error);
       }
     };
@@ -51,48 +53,34 @@ const NewArrivals = ({ onNewArrivalsUpdate }) => {
 
   const handlePageChange = (event, page) => {
     setCurrentPage(page);
-    setLoading(true); // Define o loading como true ao mudar a página
   };
-
-  const removeAccents = (name) => {
-    return name
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .toLowerCase()
-      .replace(/\s+/g, "-")
-      .replace(/[^\w\-]+/g, "");
-  };
-
+// Função para remover acentos
+const removeAccents = (name) => {
+  return name
+  .normalize("NFD") // Normaliza a string para decompor caracteres acentuados
+  .replace(/[\u0300-\u036f]/g, "") // Remove os diacríticos (acentos)
+  .toLowerCase() // Converte para letras minúsculas
+  .replace(/\s+/g, "-") // Substitui espaços por hífens
+  .replace(/[^\w\-]+/g, ""); // Remove caracteres não alfanuméricos (exceto hífens)
+};
+  
   return (
     <div>
+   
       <Navbar />
       {loading ? (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "10rem", marginBottom:"10rem" }}>
-          <CircularProgress /> {/* Spinner de loading */}
-        </div>
+        <NewArrivalsSkeleton /> // Exibir carregamento enquanto os dados não são carregados
       ) : (
         <div style={{ marginTop: "2rem" }}>
-          <h1
-            style={{
-              marginBottom: "1rem",
-              fontFamily: "poppins, sans serif",
-              fontSize: "1.2rem ",
-              marginLeft: "1rem",
-            }}
-          >
-            Novidades
-          </h1>
+                <h1 style={{ marginBottom: "1rem",fontFamily:"poppins, sans serif", fontSize: "1.2rem ",  marginLeft:"1rem",  }}>Novidades</h1>
 
           <ul className="ulContainer">
             {newArrivals.map((product, index) => (
               <li key={product._id} className="liContainer">
                 <div className="IconToggleContainer">
-                  <IconToggle productId={product._id} />
+                  <IconToggle productId={product._id}  />
                 </div>
-                <Link
-                  to={`/products/${removeAccents(product.name)}/${product._id}`}
-                  className="LinkContainer"
-                >
+                <Link to={`/products/${removeAccents(product.name)}/${product._id}`} className="LinkContainer">
                   {product.variations &&
                     product.variations[0] &&
                     product.variations[0].urls && (
@@ -114,7 +102,8 @@ const NewArrivals = ({ onNewArrivalsUpdate }) => {
                           product.variations[0].urls.length > 1 &&
                           setHoveredIndex(-1)
                         }
-                        loading="lazy"
+                        loading="lazy" // Adiciona lazy loading
+
                       />
                     )}
                   <div style={{ display: "flex", flexDirection: "column" }}>
@@ -124,10 +113,16 @@ const NewArrivals = ({ onNewArrivalsUpdate }) => {
                         fontWeight: "700",
                         fontFamily: "poppins, sans-serif",
                       }}
+                      
                     >
                       R$ {product.variations[0].sizes[0].price}
                     </span>
-                    <span className="ulContainer__name">{product.name}</span>
+                    <span
+                    
+                      className="ulContainer__name"
+                    >
+                      {product.name}
+                    </span>
                   </div>
                 </Link>
               </li>
